a43953dffb08fcf564eaf9f61bbbc2db6777504c
Wan Nabil <wannabil.xyz@gmail.com>
2023-05-24 05:05:17 +0800

    3 out 5 item done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const SearchFilter = () => { 
+const [userData, setUserData] = useState([
+  {id: 1, name: Nabil, email: nabil@hello.com},
+  {id: 2, name: Ali, email: ali@hello.com},
+  {id: 3, name: Hasan, email: hasan@hello.com};
+]);
+const [filteredResults, setFilteredResults] = useState([]);
+const [searchName, setSearchName] = useState('');
+
+const searchItem = (e) => {
+  const searchTerm = e.target.value;
+  setSearchName(searchTerm);
+
+const filteredData = userData.filter((user) => {
+  return user.name.toLowerCase().includes(searchTerm.toLowerCase());
+});
+
+  setFilteredResults(filteredData);
+};
+
+}
 
 function App() {
   return (
@@ -9,14 +32,18 @@ function App() {
         <p>
         You will build an app that allows users to enter search keys and it filters the user list from the hardcode list based on the entered key
         </p>
+
       </header>
     </div>
   );
 }
 
+
+
 export default App;
 
-// dataset, id: name: email:
-// e.target.value
-// use filter
+// dataset, id: name: email: done (1/5)
+// e.target.value done (2/5)
+// use filter done (3/5)
 // .map
+// key id to display
